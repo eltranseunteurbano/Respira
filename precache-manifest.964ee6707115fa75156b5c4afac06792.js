f10ca8ca130145eef429ad111b76d98f15aebec3
Jaime Burbano <burbanojaime98@gmail.com>
2020-05-13 03:02:10 -0500

    Updates

---
+++ precache-manifest.964ee6707115fa75156b5c4afac06792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f41dde9cf56b8a112510acd0635a00",
+    "url": "/Respira/index.html"
+  },
+  {
+    "revision": "147b015f1e81096bf359",
+    "url": "/Respira/static/css/main.3d537b83.chunk.css"
+  },
+  {
+    "revision": "203c2b85116921b5b15b",
+    "url": "/Respira/static/js/2.2b825f24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Respira/static/js/2.2b825f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "147b015f1e81096bf359",
+    "url": "/Respira/static/js/main.cbb5484c.chunk.js"
+  },
+  {
+    "revision": "1e18bb97efee5f9eae5a",
+    "url": "/Respira/static/js/runtime-main.df3d74f6.js"
+  }
+]);
